f29428c82f9a48beb3f6d4e7a82eee454e4d05d0
Charlie Gorichanaz <charlie@gorichanaz.com>
2017-04-02 03:19:24 -0700

    First pass at dividing modules

--- mods-available/zsh/completion.zsh
+++ mods-available/zsh/completion.zsh
@@ -3,3 +3,9 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# Automatically quote globs in URL and remote references
+__remote_commands=(scp rsync)
+autoload -U url-quote-magic
+zle -N self-insert url-quote-magic
+zstyle -e :urlglobber url-other-schema '[[ $__remote_commands[(i)$words[1]] -le ${#__remote_commands} ]] && reply=("*") || reply=(http https ftp)'
