4e24a6bc945705599722217e1957662da3fee58e
CNG <charlie@gorichanaz.com>
2017-04-29 21:08:30 -0700

    Laptop edits

--- mods-available/zsh/env.path-local.zsh
+++ mods-available/zsh/env.path-local.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$PATH"
-export MANPATH="/usr/local/man:$MANPATH"
+export PATH=./bin:/usr/local/bin:/usr/local/sbin:$PATH
+export MANPATH=/usr/local/man:$MANPATH
