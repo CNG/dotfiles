a6f3b8122603f83f6e0c8facc3921936ce123686
Charlie Gorichanaz <charlie@gorichanaz.com>
2017-11-20 00:53:51 -0800

    Fixes over past months

--- mods-available/zsh/env.path-local.zsh
+++ mods-available/zsh/env.path-local.zsh
@@ -1,2 +1,4 @@
-export PATH=./bin:/usr/local/bin:/usr/local/sbin:$PATH
+# forgot why I wanted ./bin first, but it was causing issue with caskrepair
+# export PATH=./bin:/usr/local/bin:/usr/local/sbin:$PATH
+export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 export MANPATH=/usr/local/man:$MANPATH
