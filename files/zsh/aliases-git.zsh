0b5cb1f54591bb03bfe11d6e8418735e2a347070
Charlie Gorichanaz <charlie@gorichanaz.com>
2018-06-30 13:49:02 -0700

    Open terminal on release

--- files/zsh/aliases-git.zsh
+++ files/zsh/aliases-git.zsh
@@ -16,6 +16,7 @@ alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
 alias gc='git commit'
 alias gca='git commit -a'
+alias gcam='git commit --amend --no-edit'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
