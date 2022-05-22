3beb4295274a1223465f5b88df423baacd3d5987
Charlie Gorichanaz <charlie@gorichanaz.com>
2022-05-22 14:05:31 -0700

    Clean up Vim config and move to vim-plug for easier setup

--- files/vim/global_extra_conf.py
+++
@@ -1,7 +0,0 @@
-# This is for YouCompleteMe Python Vim completion
-def Settings(**kwargs):
-    client_data = kwargs['client_data']
-    return {
-        'interpreter_path': client_data['g:ycm_python_interpreter_path'],
-        'sys_path': client_data['g:ycm_python_sys_path'],
-    }
