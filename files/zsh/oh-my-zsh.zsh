38479a3cc2b51e93f62de7bc84340a8039b49dae
Charlie Gorichanaz <charlie@gorichanaz.com>
2022-07-07 15:19:33 -0700

    Remove i3-gaps, adjust tmux for nesting, rename history zsh

--- files/zsh/oh-my-zsh.zsh
+++ files/zsh/oh-my-zsh.zsh
@@ -14,6 +14,6 @@ COMPLETION_WAITING_DOTS="true"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(${plugins:-} history-search-multi-word)
+plugins=(${plugins:-} H-S-MW)
 
 source $ZSH/oh-my-zsh.sh
