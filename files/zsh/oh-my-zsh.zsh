ebcfef8c36228e21294c7cb93c0c81e7ee5683dd
Charlie Gorichanaz <charlie@gorichanaz.com>
2024-03-29 15:59:45 -0700

    More updates

--- files/zsh/oh-my-zsh.zsh
+++ files/zsh/oh-my-zsh.zsh
@@ -1,12 +1,13 @@
 # Path to your oh-my-zsh configuration.
-ZSH=$HOME/.oh-my-zsh
+ZSH=/usr/share/oh-my-zsh/
+ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Set to this to use case-sensitive completion
 CASE_SENSITIVE="true"
 
-DISABLE_UPDATE_PROMPT=true
+zstyle ':omz:update' mode disabled  # disable automatic updates
 
 # Uncomment following line if you want red dots to be displayed while waiting for completion
 COMPLETION_WAITING_DOTS="true"
@@ -14,6 +15,6 @@ COMPLETION_WAITING_DOTS="true"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(${plugins:-} H-S-MW)
+plugins=(${plugins:-} history-search-multi-word)
 
 source $ZSH/oh-my-zsh.sh
